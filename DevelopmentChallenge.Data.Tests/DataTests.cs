8b7feeb43184a3fcb9f8156087c5a0ab7f466e29
Andres <andres>
2024-05-20 11:35:43 -0300

    Add project files.

---
+++ DevelopmentChallenge.Data.Tests/DataTests.cs
@@ -0,0 +1,145 @@
+﻿using DevelopmentChallenge.Data.Classes;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DevelopmentChallenge.Data.Tests
+{
+    [TestFixture]
+    public class DataTests
+    {
+        [TestCase]
+        public void TestResumenListaVacia()
+        {
+            Assert.AreEqual("<h1>Lista vacía de formas!</h1>",
+                ReporteFormas.Imprimir(new List<FormaGeometrica>(), Idioma.Castellano));
+        }
+
+        [TestCase]
+        public void TestResumenListaVaciaFormasEnIngles()
+        {
+            Assert.AreEqual("<h1>Empty list of shapes!</h1>",
+                ReporteFormas.Imprimir(new List<FormaGeometrica>(), Idioma.Ingles));
+        }
+
+        [TestCase]
+        public void TestResumenListaVaciaFormasEnItaliano()
+        {
+            Assert.AreEqual("<h1>Elenco vuoto di moduli!</h1>",
+                ReporteFormas.Imprimir(new List<FormaGeometrica>(), Idioma.Italiano));
+        }
+
+        [TestCase]
+        public void TestResumenListaConUnCuadrado()
+        {
+            var cuadrados = new List<FormaGeometrica> {new Cuadrado(5)};
+
+            var resumen = ReporteFormas.Imprimir(cuadrados, Idioma.Castellano);
+
+            Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConUnTrapecioEnItaliano()
+        {
+            var trapecios = new List<FormaGeometrica> { new TrapecioRectangulo(1, 3, 5) };
+
+            var resumen = ReporteFormas.Imprimir(trapecios, Idioma.Italiano);
+
+            Assert.AreEqual("<h1>Rapporto sui moduli</h1>1 Trapezio | Area 10 | Perimetro 14,2 <br/>TOTALE:<br/>1 moduli Perimetro 14,2 Area 10", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasCuadrados()
+        {
+            var cuadrados = new List<FormaGeometrica>
+            {
+                new Cuadrado(5),
+                new Cuadrado(1),
+                new Cuadrado(3)
+            };
+
+            var resumen = ReporteFormas.Imprimir(cuadrados, Idioma.Ingles);
+
+            Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasTrapeciosEnItaliano()
+        {
+            var trapecios = new List<FormaGeometrica>
+            {
+                new TrapecioRectangulo(1, 3, 5),
+                new TrapecioRectangulo(2, 2, 4),
+                new TrapecioRectangulo(1.2m, 3, 2.4m)
+            };
+
+            var resumen = ReporteFormas.Imprimir(trapecios, Idioma.Italiano);
+
+            Assert.AreEqual("<h1>Rapporto sui moduli</h1>3 Trapezi | Area 23,04 | Perimetro 35,52 <br/>TOTALE:<br/>3 moduli Perimetro 35,52 Area 23,04", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasTipos()
+        {
+            var formas = new List<FormaGeometrica>
+            {
+                new Cuadrado(5),
+                new Circulo(3),
+                new TrianguloEquilatero(4),
+                new Cuadrado(2),
+                new TrianguloEquilatero(9),
+                new Circulo(2.75m),
+                new TrianguloEquilatero(4.2m)
+            };
+
+            var resumen = ReporteFormas.Imprimir(formas, Idioma.Ingles);
+
+            Assert.AreEqual(
+                "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13.01 | Perimeter 18.06 <br/>3 Triangles | Area 49.64 | Perimeter 51.6 <br/>TOTAL:<br/>7 shapes Perimeter 97.66 Area 91.65",
+                resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasTiposEnCastellano()
+        {
+            var formas = new List<FormaGeometrica>
+            {
+                new Cuadrado(5),
+                new Circulo(3),
+                new TrianguloEquilatero(4),
+                new Cuadrado(2),
+                new TrianguloEquilatero(9),
+                new Circulo(2.75m),
+                new TrianguloEquilatero(4.2m)
+            };
+
+            var resumen = ReporteFormas.Imprimir(formas, Idioma.Castellano);
+
+            Assert.AreEqual(
+                "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65",
+                resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasTiposEnItaliano()
+        {
+            var formas = new List<FormaGeometrica>
+            {
+                new Cuadrado(3),
+                new Circulo(7),
+                new TrianguloEquilatero(4),
+                new TrapecioRectangulo(2, 6, 2),
+                new TrianguloEquilatero(6),
+                new Circulo(3),
+                new TrapecioRectangulo(2, 6, 2),
+                new TrianguloEquilatero(1.2m)
+            };
+
+            var resumen = ReporteFormas.Imprimir(formas, Idioma.Italiano);
+
+            Assert.AreEqual(
+                "<h1>Rapporto sui moduli</h1>1 Quadrato | Area 9 | Perimetro 12 <br/>2 Cerchi | Area 45,55 | Perimetro 31,42 <br/>3 Triangoli | Area 23,14 | Perimetro 33,6 <br/>2 Trapezi | Area 16 | Perimetro 27,31 <br/>TOTALE:<br/>8 moduli Perimetro 104,33 Area 93,69",
+                resumen);
+        }
+    }
+}
---
+++ DevelopmentChallenge.Data/Classes/FormaGeometrica.cs
@@ -0,0 +1,229 @@
+﻿/******************************************************************************************************************/
+/******* ¿Qué pasa si debemos soportar un nuevo idioma para los reportes, o agregar más formas geométricas? *******/
+/******************************************************************************************************************/
+
+/*
+ * TODO: 
+ * Refactorizar la clase para respetar principios de la programación orientada a objetos.
+ * Implementar la forma Trapecio/Rectangulo. 
+ * Agregar el idioma Italiano (o el deseado) al reporte.
+ * Se agradece la inclusión de nuevos tests unitarios para validar el comportamiento de la nueva funcionalidad agregada (los tests deben pasar correctamente al entregar la solución, incluso los actuales.)
+ * Una vez finalizado, hay que subir el código a un repo GIT y ofrecernos la URL para que podamos utilizar la nueva versión :).
+ */
+
+using DevelopmentChallenge.Data.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DevelopmentChallenge.Data.Classes
+{
+    public abstract class FormaGeometrica
+    {
+        public TipoForma TipoForma { get; protected set; }
+        public abstract decimal CalcularArea();
+        public abstract decimal CalcularPerimetro();
+    }
+
+    public class Cuadrado : FormaGeometrica
+    {
+        private readonly decimal _lado;
+
+        public Cuadrado(decimal lado)
+        {
+            _lado = lado;
+            TipoForma = TipoForma.Cuadrado;
+        }
+
+        public override decimal CalcularArea()
+        {
+            return _lado * _lado;
+        }
+
+        public override decimal CalcularPerimetro()
+        {
+            return _lado * 4;
+        }
+    }
+
+    public class Circulo : FormaGeometrica
+    {
+        private readonly decimal _radio;
+
+        public Circulo(decimal radio)
+        {
+            _radio = radio;
+            TipoForma = TipoForma.Circulo;
+        }
+
+        public override decimal CalcularArea()
+        {
+            return (decimal)Math.PI * (_radio / 2) * (_radio / 2);
+        }
+
+        public override decimal CalcularPerimetro()
+        {
+            return (decimal)Math.PI * _radio;
+        }
+    }
+
+    public class TrianguloEquilatero : FormaGeometrica
+    {
+        private readonly decimal _lado;
+
+        public TrianguloEquilatero(decimal lado)
+        {
+            _lado = lado;
+            TipoForma = TipoForma.TrianguloEquilatero;
+        }
+
+        public override decimal CalcularArea()
+        {
+            return ((decimal)Math.Sqrt(3) / 4) * _lado * _lado;
+        }
+
+        public override decimal CalcularPerimetro()
+        {
+            return _lado * 3;
+        }
+    }
+
+    public class TrapecioRectangulo : FormaGeometrica
+    {
+        private readonly decimal _baseMayor;
+        private readonly decimal _baseMenor;
+        private readonly decimal _altura;
+
+        public TrapecioRectangulo(decimal baseMayor, decimal baseMenor, decimal altura)
+        {
+            _baseMayor = baseMayor;
+            _baseMenor = baseMenor;
+            _altura = altura;
+            TipoForma = TipoForma.TrapecioRectangulo;
+        }
+
+        public override decimal CalcularArea()
+        {
+            return ((_baseMayor + _baseMenor) * _altura) / 2;
+        }
+
+        public override decimal CalcularPerimetro()
+        {
+            var ladoRecto = Math.Abs(_baseMayor - _baseMenor) / 2;
+            var ladoOblicuo = (decimal)Math.Sqrt((double)(_altura * _altura + ladoRecto * ladoRecto));
+            return _baseMayor + _baseMenor + 2 * ladoOblicuo;
+        }
+    }
+
+    public class ReporteFormas
+    {
+        public static string Imprimir(List<FormaGeometrica> formas, Idioma idioma)
+        {
+            SetCulture(idioma);
+
+            var sb = new StringBuilder();
+
+            if (!formas.Any())
+            {
+                sb.Append($"<h1>{Resources.ListaVaciaDeFormas}</h1>");
+            }
+            else
+            {
+                sb.Append($"<h1>{Resources.ReporteDeFormas}</h1>");
+                sb.Append(GenerarReportePorForma(formas));
+                sb.Append(GenerarFooter(formas));
+            }
+
+            return sb.ToString();
+        }
+
+        private static void SetCulture(Idioma idioma)
+        {
+            var culture = new CultureInfo(GetEnumerationDescription(idioma));
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+
+        private static string GenerarReportePorForma(List<FormaGeometrica> formas)
+        {
+            var sb = new StringBuilder();
+            var tiposDeFormas = Enum.GetValues(typeof(TipoForma)).Cast<TipoForma>();
+
+            foreach (var tipoForma in tiposDeFormas)
+            {
+                var formasTipo = formas.Where(f => f.TipoForma == tipoForma).ToList();
+
+                if (formasTipo.Any())
+                {
+                    var cantidad = formasTipo.Count;
+                    var areaTotal = formasTipo.Sum(f => f.CalcularArea());
+                    var perimetroTotal = formasTipo.Sum(f => f.CalcularPerimetro());
+
+                    sb.Append($"{cantidad} {TraducirForma(tipoForma, cantidad)} | {Resources.Area} {areaTotal:#.##} | {Resources.Perimetro} {perimetroTotal:#.##} <br/>");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static string GenerarFooter(List<FormaGeometrica> formas)
+        {
+            var sb = new StringBuilder();
+            var totalFormas = formas.Count;
+            var areaTotal = formas.Sum(f => f.CalcularArea());
+            var perimetroTotal = formas.Sum(f => f.CalcularPerimetro());
+
+            sb.Append($"{Resources.Total.ToUpper()}:<br/>");
+            sb.Append($"{totalFormas} {Resources.Formas} ");
+            sb.Append($"{Resources.Perimetro} {perimetroTotal:#.##} ");
+            sb.Append($"{Resources.Area} {areaTotal:#.##}");
+
+            return sb.ToString();
+        }
+
+        private static string TraducirForma(TipoForma tipo, int cantidad)
+        {
+            var traducciones = new Dictionary<TipoForma, (string singular, string plural)>
+            {
+                { TipoForma.Cuadrado, (Resources.Cuadrado, Resources.Cuadrados) },
+                { TipoForma.Circulo, (Resources.Circulo, Resources.Circulos) },
+                { TipoForma.TrianguloEquilatero, (Resources.Triangulo, Resources.Triangulos) },
+                { TipoForma.TrapecioRectangulo, (Resources.Trapecio, Resources.Trapecios) }
+            };
+
+            var (singular, plural) = traducciones[tipo];
+
+            return cantidad == 1 ? singular : plural;
+        }
+
+        private static string GetEnumerationDescription(Enum value)
+        {
+            var fi = value.GetType().GetField(value.ToString());
+            var attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            return attributes?.FirstOrDefault()?.Description ?? value.ToString();
+        }
+    }
+
+    public enum Idioma
+    {
+        [Description("es")]
+        Castellano = 1,
+        [Description("en")]
+        Ingles,
+        [Description("it")]
+        Italiano
+    }
+
+    public enum TipoForma
+    {
+        Cuadrado = 1,
+        Circulo,
+        TrianguloEquilatero,
+        TrapecioRectangulo
+    }
+}
